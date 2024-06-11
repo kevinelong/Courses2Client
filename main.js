35ce6906d4763797c45c736bc0cedab743443e73
kevinelong <kevinelong@gmail.com>
2024-06-11 06:37:46 -0700

    delete from read

--- main.js
+++ main.js
@@ -3,3 +3,26 @@ function getCourseId() {
     const id = parseInt(params.get("id"));
     return id;
 } 
+
+function renderCourse(course, targetId){
+    // window.courseId.innerText = JSON.stringify(course, undefined, 4)
+    const target = document.getElementById(targetId);
+    target.innerHTML = `
+        <h2>${course.courseName}</h2>
+        <span class="id">ID: ${course.id}</span>,
+        <span class="dept">DEPT: ${course.dept}</span>,
+        <span class="number">NUMBER: ${course.courseNum}</span>
+        <h2 class="instructor">INSTRUCTOR: ${course.instructor}</h2>
+        <span class="number">DATE: ${course.startDate}</span>
+        <h2 class="days">DAYS:${course.numDays}</h2>
+    `;
+}
+
+function readCourse(targetId){
+    // window.courseId.innerText = getCourseId();
+
+    const baseURL = "http://localhost:8081/api";
+    const coursesURI = "/courses/" + getCourseId();
+
+    fetch(baseURL + coursesURI).then(r=>r.json()).then( c => renderCourse(c, targetId) );
+}
